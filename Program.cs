3a70e5a4c841b4e99dfb38394de88cc0db0e9298
Shintaxx <eyeofcthulhu27@gmail.com>
2023-02-12 21:41:25 +0300

    Added a loop with a condition check

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿string[] firstArray = new string[3] {"-2", "Denmark", "123"};
+string[] secondArray = new string[firstArray.Length];
+void Function(string[] firstArray, string[] secondArray)
+{
+    int score = 0;
+    for (int q = 0; q < firstArray.Length; q++)
+    {
+    if(firstArray[q].Length <= 3)
+        {
+        secondArray[score] = firstArray[q];
+        score++;
+        }
+    }
+}
